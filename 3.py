3a25f3c02da14245063f7fbc77eaf5bfedab3db6
lisavere <verepekinaelizabeth@yandex.ru>
2020-01-23 02:20:51 +0700

    commit

---
+++ 3.py
@@ -0,0 +1,20 @@
+﻿import math
+from selenium import webdriver
+link = "http://suninjuly.github.io/selects1.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+def calc(x,y):
+  return str((int(x)+int(y)))
+
+x = browser.find_element_by_id("num1").text
+y = browser.find_element_by_id("num2").text
+
+
+z = calc(x,y)
+
+browser.find_element_by_tag_name("select").click()
+browser.find_element_by_css_selector("[value='" + z + "']").click()
+
+option3 = browser.find_element_by_tag_name('button')
+option3.submit()
---
+++ 5.py
@@ -0,0 +1,18 @@
+﻿from selenium import webdriver
+import os 
+link = "http://suninjuly.github.io/file_input.html"
+driver = webdriver.Chrome()
+driver.get(link)
+
+input1 = driver.find_element_by_name('firstname')
+input1.send_keys("Елизавета")
+input2 = driver.find_element_by_name('lastname')
+input2.send_keys("Верепекина")
+input3 = driver.find_element_by_name('email')
+input3.send_keys("verepekinaelizabeth@yandex.ru")
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+file_path = os.path.join(current_dir, 'file.txt') 
+
+option3 = driver.find_element_by_tag_name('button')
+option3.submit()
